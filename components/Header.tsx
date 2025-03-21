641c195dac22769956fc649c9103209824618083
Nguyễn Tạ Minh Trung <trungntm.97@gmail.com>
2025-03-21 16:01:39 +0700

    Update color UI and enable tag menu (#87)

--- components/Header.tsx
+++ components/Header.tsx
@@ -31,7 +31,7 @@ const Header = () => {
   return (
     <div className={'mx-auto max-w-3xl sm:px-6 xl:max-w-5xl xl:px-0'}>
       <header
-        className={`fixed z-50 w-full max-w-3xl bg-white px-4 sm:pr-16 sm:pl-6 md:px-8 xl:max-w-5xl xl:px-0 dark:bg-gray-950`}
+        className={`fixed z-50 w-full max-w-3xl bg-white px-4 sm:pr-16 sm:pl-6 md:px-8 xl:max-w-5xl xl:px-0 dark:bg-gray-900`}
       >
         <div
           id={'nav'}
--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: 'Home', active: true },
   { href: '/blog', title: 'Blog', active: true },
-  { href: '/tags', title: 'Tags', active: false },
+  { href: '/tags', title: 'Tags', active: true },
   { href: '/projects', title: 'Projects', active: false },
   { href: '/about', title: 'About', active: true },
   { href: '/resume', title: 'Resume', active: false },
