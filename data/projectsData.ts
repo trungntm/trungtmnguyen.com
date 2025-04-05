e9b906ad64feab992d6788d0bec1ad0d427ec64f
trungntm <trungntm.97@gmail.com>
2025-04-05 19:05:25 +0700

    Run lint and format

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -27,20 +27,19 @@ const projectsData: Project[] = [
     id: 1,
     title: 'Express-boot',
     href: '#',
-    description:
-      'Speed ​​up Express app development through auto-configuration.',
+    description: 'Speed ​​up Express app development through auto-configuration.',
     date: 'Apr 04, 2025',
     datetime: '2025-04-04',
     category: { title: 'development', href: '#' },
-    imgSrc:'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    imgSrc:
+      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
     author: {
       name: 'Nguyen Ta Minh Trung',
       role: '',
       href: '#',
-      imageUrl:
-        '/static/images/pages/avatar.jpg',
+      imageUrl: '/static/images/pages/avatar.jpg',
     },
-  }
+  },
 ]
 
 export default projectsData
