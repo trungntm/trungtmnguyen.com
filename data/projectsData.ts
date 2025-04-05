ea0a69169f21ea2b995fc444bc065665eac53bbb
Nguyễn Tạ Minh Trung <trungntm.97@gmail.com>
2025-04-05 18:59:32 +0700

    Add project card image component and mock data (#102)

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,27 +1,46 @@
+interface ProjectCategory {
+  title: string
+  href: string
+}
+
+interface ProjectAuthor {
+  name: string
+  role: string
+  href: string
+  imageUrl: string
+}
+
 interface Project {
+  id: number
   title: string
   description: string
   href?: string
+  date?: string
+  datetime: string
+  category?: ProjectCategory
   imgSrc?: string
+  author: ProjectAuthor
 }
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
-  },
+    id: 1,
+    title: 'Express-boot',
+    href: '#',
+    description:
+      'Speed ​​up Express app development through auto-configuration.',
+    date: 'Apr 04, 2025',
+    datetime: '2025-04-04',
+    category: { title: 'development', href: '#' },
+    imgSrc:'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    author: {
+      name: 'Nguyen Ta Minh Trung',
+      role: '',
+      href: '#',
+      imageUrl:
+        '/static/images/pages/avatar.jpg',
+    },
+  }
 ]
 
 export default projectsData
