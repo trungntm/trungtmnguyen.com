6070cfad7d5f17ccb17479046b228af6c58c44cf
Nguyễn Tạ Minh Trung <trungntm.97@gmail.com>
2025-04-16 23:09:15 +0700

    Update function response type (#108)

--- app/api/timeline/route.ts
+++ app/api/timeline/route.ts
@@ -2,8 +2,8 @@ import { NextResponse } from 'next/server'
 
 import workingTimeline from '@/data/workingTimeline'
 
-const handler = () => {
-  return NextResponse.json(workingTimeline)
+const handler = (): NextResponse => {
+  return NextResponse.json(workingTimeline as unknown)
 }
 
 export { handler as GET }
