e10fc52df535e40fa5310646ff4ceb569bfcec72
Nguyễn Tạ Minh Trung <trungntm.97@gmail.com>
2025-06-28 14:50:23 +0700

    Update working timeline (#118)

--- data/workingTimeline.ts
+++ data/workingTimeline.ts
@@ -14,7 +14,7 @@ const workingTimelineData: TimeLine[] = [
   {
     company: 'KMS Technology',
     role: 'Senior Software Engineer',
-    duration: 'Jun 2019 – Present',
+    duration: 'Jun 2019 - Present',
     description: [
       'Joining KMS as a Software Engineer at Jun 2019.',
       'Becoming a Senior Software Engineer at Jan 2022.',
@@ -24,16 +24,36 @@ const workingTimelineData: TimeLine[] = [
     projects: [
       {
         company: 'KMS Technology',
+        project: 'LNI - ILAAM',
+        role: 'Senior Software Engineer',
+        duration: 'Dec 2024 - Present',
+        description: [
+          'Being in-charged of Insurance context service - the important service to return insurance context for others services in eco-system, from new features, bug fixing.',
+          'Onboarding new product profiles to system to ensuring the insurance context is returned correctly.',
+          'Brainstorming ideas, architect structures to enhance Insurance context service. Develop caching strategy by using memcache & redis to reduce the response time.',
+          'Exposing the libraries to other teams to use - which help them to interact with Insurance context service easily.',
+          'Being in-charged of performance issues of all services - investigating, detecting the root cause and improve performance of systems.',
+          'Using github-actions, helm chart to deploy the services to the AKS.',
+          'Mentoring junior members and help them catch-up the systems.',
+          'Skill set utilized: Java, Spring Boot, Azure, micro-service architecture.',
+          'Team size: 10',
+        ],
+        links: [],
+        logo: '/static/logo/LNI.png',
+      },
+      {
+        company: 'KMS Technology',
         project: 'LNI - CPP',
         role: 'Senior Software Engineer',
         duration: 'Jul 2024 - Dec 2024',
         description: [
           'Developing new features for files processing system.',
-          'Be in-charged of performance issues, ensure the large files can be processed without any performance issues.',
+          'Being in-charged of performance issues, ensure the large files can be processed without any performance issues.',
           'Developing Time Estimator service to provide the estimated time for the files to be processed.',
           'Using github-actions, helm chart to deploy the services to the AKS.',
           'Building Datatable component for the frontend using ReactJs, this help developers reduce a lot of code when rendering the table.',
           'Providing technical approaches/refactor the frontend source code by apply MUI theme, react hooks, building component reusable',
+          'Mentoring junior members and help them catch-up the systems.',
           'Skill set utilized: Java, Spring Boot, Azure, micro-service architecture, ReactJs.',
           'Team size: 10',
         ],
